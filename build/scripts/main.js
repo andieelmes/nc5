df502b5788d544234389061f192c7f6f0e379a64
andieelmes <andie.elmes@gmail.com>
2018-08-21 19:44:07 +0200

    add: partners and partners slider

---
+++ build/scripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: auto,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  })
+});
